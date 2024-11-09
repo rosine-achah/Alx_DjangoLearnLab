53a8b0386f7d1d440d15a6b50f9ef3e1ad64fe88
rosine achah <achahtanga29@gmail.com>
2024-11-09 21:30:29 +0100

    commit for my third project

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+ROLE_CHOICES = [
+    ("Admin", "Admin"),
+    ("Librarian", "Librarian"),
+    ("Member", "Member"),
+]
+
 
 # Create your models here.
 class Author(models.Model):
@@ -13,8 +21,8 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title 
-    
+        return self.title
+
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
@@ -23,11 +31,15 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
-    library = models.OneToOneField(Library, on_delete= models.CASCADE)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="Member")
 
     def __str__(self):
         return self.name
-
- 
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -42,3 +42,41 @@ def register(request):
         form = UserCreationForm()
 
     return render(request, "relationship_app/register.html", {"form": form})
+
+
+# views.py
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+
+# Helper function to check if the user is an Admin
+def is_admin(user):
+    return user.userprofile.role == "Admin"
+
+
+# Helper function to check if the user is a Librarian
+def is_librarian(user):
+    return user.userprofile.role == "Librarian"
+
+
+# Helper function to check if the user is a Member
+def is_member(user):
+    return user.userprofile.role == "Member"
+
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "admin_view.html")
+
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "librarian_view.html")
+
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "member_view.html")
