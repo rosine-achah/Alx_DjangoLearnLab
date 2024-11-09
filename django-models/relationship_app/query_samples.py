e73cb9d9a45fbde97a1d1d3db0bd9aefd538c7dd
rosine achah <achahtanga29@gmail.com>
2024-11-09 14:57:07 +0100

    the first commit this week

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from relationship_app.models import *
 
 def get_book(author_name):
     author = Author.objects.get(name=author_name)
-    return Book.object.filter(author=author)
+    return Book.objects.filter(author=author)
 
 def list_all_books(library_name):
     library = Library.objects.get(name=library_name)
