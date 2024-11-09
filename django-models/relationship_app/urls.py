eb436eb58f94c0e2c9c545c3a3e7a86c2da9ad97
rosine achah <achahtanga29@gmail.com>
2024-11-09 19:30:18 +0100

    commit for task one

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path 
-from . import views 
-from .views import book_list
-from .views import LibraryDetails
+from django.urls import path
+from . import views
+from .views import list_books, LibraryDetails
 
 urlpatterns = [
-     path('books/', list_books, name = "list_books")
-     path('library/', LibraryDetails, name = "LibraryDetails" )
+    path("books/", list_books, name="list_books"),
+    path("library/", LibraryDetails, name="LibraryDetails"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Library
 
 
 # Create your views here.
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     context = {"books": books}
     return render(request, "relationship_app/list_books.html", context)
