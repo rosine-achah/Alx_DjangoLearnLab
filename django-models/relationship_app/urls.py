78a350943152625474b4de988d04ae97ee26ea73
rosine achah <achahtanga29@gmail.com>
2024-11-09 21:54:35 +0100

    my last project commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("admin/", views.admin_view, name="admin_view"),
     path("librarian/", views.librarian_view, name="librarian_view"),
     path("member/", views.member_view, name="member_view"),
-    path("book/add/", views.add_book, name="add_book"),
-    path("book/edit/<int:pk>/", views.edit_book, name="edit_book"),
-    path("book/delete/<int:pk>/", views.delete_book, name="delete_book"),
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
