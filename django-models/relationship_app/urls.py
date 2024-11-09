98e3ca7c97df04fc5897e8db042ac049d33d9e0b
rosine achah <achahtanga29@gmail.com>
2024-11-09 20:32:45 +0100

    commit for project two

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, LoginView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/", LibraryDetailView, name="LibraryDetailView"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/".LogoutView.as_view(template_name="logout.html"), name="logout"),
+    path("register/", views.register, name="register"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -16,3 +21,24 @@ class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
 
     context_object_name = Library
+
+
+class LoginView(CreateView):
+    pass
+
+
+class LogoutView(CreateView):
+    next_page = reverse_lazy(login)
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, "register.html", {"form": form})
