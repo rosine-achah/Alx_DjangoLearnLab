79b82b573995078aee73e77a7e897efcfbd6e79e
rosine achah <achahtanga29@gmail.com>
2024-11-24 03:50:28 +0100

    first

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -7,3 +7,14 @@ def search_books(request):
     query = request.GET.get("query", "")
     books = Book.objects.filter(title__icontains=query)
     return render(request, "bookshelf/book_list.html", {"books": books})
+
+
+from .forms import BookSearchForm
+
+
+def search_books(request):
+    form = BookSearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data["query"]
+        books = Book.objects.filter(title__icontains=query)
+    return render(request, "bookshelf/book_list.html", {"form": form, "books": books})
