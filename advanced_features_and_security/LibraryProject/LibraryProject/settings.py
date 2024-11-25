ad0d20fa07f23e2c2ab63dbf23561380aca9db6c
rosine achah <achahtanga29@gmail.com>
2024-11-26 00:43:46 +0100

    first

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-njt(5p(z@1f7$_t344i&%v5^#9pv=_&&25x9$no--ja64bx3$s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
