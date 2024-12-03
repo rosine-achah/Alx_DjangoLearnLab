476764cc8918394eba9bbefb57f5836854400d85
rosine achah <achahtanga29@gmail.com>
2024-12-03 18:08:37 +0100

    taskone

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,84 @@
+from rest_framework import serializers
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.response import Response
+from rest_framework import filters
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
+
+
+# List view for retrieving all books
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["publication_year"]
+    ordering = ["publication_year"]
+
+
+# Detail view for retrieving a single book by its ID
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Create view for adding a new book
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+# Update view for modifying an existing book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+
+# Delete view for removing a book
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+
+# # ListView for all books
+# class BookListView(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+
+# # class BookDetailView
+# class BookDetailView(generics.RetrieveAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#     lookup_field = "pk"
+
+
+# # CreateView for adding a new book
+# class BookCreateView(generics.CreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+#     def perform_create(self, serializer):
+
+#         user = self.request.user
+#         serializer.save(user=user)
+#         return super().perform_create(serializer)
+
+
+# # UpdateView for modifying an exisitng book
+# class BookUpdateView(generics.UpdateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+
+# # DeleteView for removing a book
+# class BookDeleteView(generics.DestroyAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
