ca9abf963e32e105d885ae2d4713364e31f4a6ce
rosine achah <achahtanga29@gmail.com>
2024-12-03 18:14:38 +0100

    taskone

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,6 +6,13 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List all books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve a single book
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+
+    # Update a book
+    path('books/update', BookUpdateView.as_view(), name = 'book-update'), 
+    # path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'), 
+
+
+    #Delete a book 
+    path('books/delete', BookDeleteView.as_view(), name='book-delete')
+    # path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
 ]
