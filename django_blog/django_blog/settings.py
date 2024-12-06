db671c36838c8854966553b56213e435290cadcc
rosine achah <achahtanga29@gmail.com>
2024-12-06 13:08:35 +0100

    taskzero

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "USER": "tangarosine",
+        "PORT": "5432",
+        #'USER': 'your_database_user',
+        #'PORT': '5432',
     }
 }
 
