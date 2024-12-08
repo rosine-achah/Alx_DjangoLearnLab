7569521bd475a89f75bc46eaff4e0ccf489fda16
rosine achah <achahtanga29@gmail.com>
2024-12-08 01:32:52 +0100

    taskzero

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "blog",
         "USER": "tangarosine",
-        "PORT": "5432",
+        "PORT": "33060",
     }
 }
 
