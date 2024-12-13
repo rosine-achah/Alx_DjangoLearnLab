83df050555d4b2cdaecbac6d12d65c3011acfac8
rosine achah <achahtanga29@gmail.com>
2024-12-13 19:18:51 +0100

    taskfour updated

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from taggit.managers import TaggableManager
 
 # from .models import Tag
 from django.apps import apps
@@ -21,7 +22,7 @@ class Post(models.Model):
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # tags = models.ManyToManyField(Tag, blank=True)  # Allow multiple tags for a post
-    tags = models.ManyToManyField(Tag, blank=True)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
